81fe6c230104733d9c84fb573b78dfd22ce62881
AndreLLPrado <andrele.prado@gmail.com>
2026-05-27 21:01:15 -0300

    fix no login

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,12 +2,25 @@ import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { Login } from './pages/Login';
 import { ChatBot } from './pages/ChatBot';
+import { useState, useEffect } from 'react';
 
 const queryClient = new QueryClient();
 
 function App() {
-  const token = localStorage.getItem('token');
+  const [token, setToken] = useState(localStorage.getItem('token'));
   
+  useEffect(() => {
+    const handleStorage = () => {
+      setToken(localStorage.getItem('token'));
+    };
+
+    window.addEventListener('storage', handleStorage);
+
+    return () => {
+      window.removeEventListener('storage', handleStorage);
+    };
+  },[])
+
   return (
     <QueryClientProvider client={queryClient}>
       <BrowserRouter>
